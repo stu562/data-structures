f7c89093105e5530d3c841fb175d7ab33fc2c384
Steven Tu <stu562@gmail.com>
2019-01-04 17:37:39 -0800

    implement functional instanitation of stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,31 @@
 var Queue = function() {
-  var someInstance = {};
-
   // Use an object with numeric keys to store values
-  var storage = {};
-
+  // var storage = {};
+  var someInstance = {};
+  var enqueueCount = 0;
+  var dequeueCount = 0;
   // Implement the methods below
-
+  //enqueue - mimics enqueue - unshifts adds to begining and increase stack size 
+  //dequee - pop the last 
   someInstance.enqueue = function(value) {
+    someInstance["'" + enqueueCount + "'"] = value;
+    enqueueCount -= 1; 
+    //add a string to the back of the queue, no return
   };
 
   someInstance.dequeue = function() {
+    if (dequeueCount > enqueueCount){
+      var temp = someInstance["'" + dequeueCount + "'"] //store data before the delete occurs
+      delete someInstance["'" + dequeueCount + "'"];
+      dequeueCount -= 1;
+    }
+    return temp;
   };
 
   someInstance.size = function() {
+    if (dequeueCount >= enqueueCount){ //base case, error protection 
+      return Math.abs(enqueueCount - dequeueCount);// converts neg to counting numbers 
+    }
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,26 +2,29 @@ var Stack = function(value) {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  // var storage = {};
+
   var counter = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
-    counter += 1;
     someInstance[counter] = value;
-    
+    counter += 1;
   };
 
   someInstance.pop = function(value) {
     if (counter > 0){
       counter -= 1;
+      var temp = someInstance[counter]
       delete someInstance[counter];
     }
+    return temp;
   };
 
-  someInstance.size = function() {
+  someInstance.size = function(value) {
     return counter;
   };
 
   return someInstance;
 };
+
