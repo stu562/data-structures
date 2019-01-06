177ce5fe17def7b2387e83f84730ff88adf00382
Steven Tu <stu562@gmail.com>
2019-01-05 22:16:25 -0800

    implement es6 style

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,25 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor() { //constructor function explicit 
+    this.enqueueCount = 0;
+    this.dequeueCount = 0;
   }
 
+  enqueue(value) {
+    this['"' + this.enqueueCount + '"'] = value;
+    this.enqueueCount -= 1; 
+  }
+  dequeue() {
+    if (this.dequeueCount > this.enqueueCount) {
+      this.temp = this['"' + this.dequeueCount + '"']; //store data before the delete occurs
+      delete this['"' + this.dequeueCount + '"'];
+      this.dequeueCount -= 1;
+    }
+    return this.temp;	
+  }
+  size() {
+    if (this.dequeueCount >= this.enqueueCount) { //base case, error protection 
+      return Math.abs(this.enqueueCount - this.dequeueCount);// converts neg to counting numbers 
+    }
+    return this.enqueueCount; // size of the enqueue that are in queue 
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,27 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor() { //constructor function explicit 
+    this.storage = {}; // prop's of newly created objects when made 
+    this.counter = 0;
+  }
+  //each newlycreated objects will have a push, pop, size method 
+  push(value) {
+    this.storage[this.counter] = value;//this points to the stacks object in the constructor
+    this.counter += 1;
   }
 
+  pop() {
+    if (this.counter > 0) {
+      this.counter -= 1;
+      var temp = this.storage[this.counter]; //store data before the delete occurs
+      delete this.storage[this.counter];
+    }
+    return temp; //get that value 
+  }
+
+  size() {
+    return this.counter;
+  }
 }
+
+
+// var someStack = new Stack();
